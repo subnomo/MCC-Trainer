fb9c4c12be95dbbe1d4e53a7a12c2d446e0407f3
Alex Taylor <alex@alext.xyz>
2019-07-22 12:14:47 -0700

    Simplify asm

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn infinite_ammo(game: &Game) {
 fn invincibility(game: &Game) {
     let mut code_cave: Vec<u8> = vec![
         0x50,                                   // push rax
-        0x48, 0xB8,                             // mov rax, 
+        0x48, 0xA1,                             // mov rax, [shield_base]
     ];
 
     let shield_base: u64 = game.base + 0x01E4489C;
@@ -43,17 +43,14 @@ fn invincibility(game: &Game) {
     }
 
     code_cave.append(&mut vec![
-        0x48, 0x8B, 0x0,                        // mov rax, [rax]
-        0x48, 0x5, 0xD8, 0x0, 0x0, 0x0,         // add rax, D8
-        0x48, 0x8B, 0x0,                        // mov rax, [rax]
-        0x48, 0x5, 0xB8, 0x5, 0x0, 0x0,         // add rax, 5B8
-        0x48, 0x8B, 0x0,                        // mov rax, [rax]
+        0x48, 0x8B, 0x80, 0xD8, 0x0, 0x0, 0x0,  // mov rax, [rax+D8]
+        0x48, 0x8B, 0x80, 0xB8, 0x5, 0x0, 0x0,  // mov rax, [rax+5B8]
         0x48, 0x5, 0x74, 0xE, 0x0, 0x0,         // add rax, E74
         0x4C, 0x3B, 0xE8,                       // cmp r13, rax
         0x58,                                   // pop rax
         0x74, 0x6,                              // je haloreach.dll+D181C4
         0xF3, 0x41, 0x0F, 0x11, 0x45, 0x10,     // mov [r13+10], xmm0
-        0xE9, 0x38, 0x60, 0x4A, 0xFF,           // jmp haloreach.dll+1BE21C ; jump back to hook
+        0xE9, 0x3F, 0x60, 0x4A, 0xFF,           // jmp haloreach.dll+1BE21C ; jump back to hook
     ]);
 
     // println!("Code cave: {:x?}", code_cave);
